6b77faafbaf834194b605477d48f91ea29290b59
lynxluu <lynxluu@gmail.com>
2022-12-06 00:04:39 +0800

    cat_mm_3buy 1

--- examples/strategies/cat_mm3buy.py
+++ examples/strategies/cat_mm3buy.py
@@ -46,6 +46,196 @@ def tas_macd_bc_V221108(c: CZSC, di: int = 1) -> OrderedDict:
     s[signal.key] = signal.value
     return s
 
+def tas_dea_cross_V221106(c: CZSC, di=55) -> OrderedDict:
+    """dea穿过0轴次数
+
+    信号逻辑：
+    连续三根macd柱子值依次增大，向上；反之，向下
+
+    信号列表：
+    - Signal('15分钟_近55根K线DEA_上穿0轴_1次_任意_任意_0')
+    - Signal('15分钟_近55根K线DEA_处于0轴以上_是_任意_任意_0')
+    - Signal('15分钟_近55根K线DEA_处于0轴以下_是_任意_任意_0')
+
+    :param c: CZSC对象
+    :param di: 连续倒3根K线
+    :return:
+    """
+
+    s = OrderedDict()
+
+    default_signals = [
+        Signal(k1=str(c.freq.value), k2=f"近{di}根K线DEA", k3="上穿0轴", v1="其他", v2='其他', v3='其他'),
+        Signal(k1=str(c.freq.value), k2=f"近{di}根K线DEA", k3="处于0轴以上", v1="其他", v2='其他', v3='其他'),
+        Signal(k1=str(c.freq.value), k2=f"近{di}根K线DEA", k3="处于0轴以下", v1="其他", v2='其他', v3='其他')
+    ]
+
+    for signal in default_signals:
+        s[signal.key] = signal.value
+
+    dea = [x.cache['MACD']['dea'] for x in c.bars_raw[-di:]]
+
+    # dea上穿0轴
+    up = down_cross_count([0 for i in range(len(dea))], dea)
+    down = down_cross_count(dea, [0 for i in range(len(dea))])
+
+    # 上穿一次0轴，最后一根K线的DEA在0轴上，倒数第5根K线DEA在0轴下
+    if up == 1 and down == 0 and dea[-1] > 0:
+        v = Signal(k1=str(c.freq.value), k2=f"近{di}根K线DEA", k3="上穿0轴", v1=f"1次")
+        s[v.key] = v.value
+
+    # 上穿一次或始终处于0轴上方，最近X根K线macd均处于0轴上方
+    if up == down == 0 and dea[-di] > 0:
+        v = Signal(k1=str(c.freq.value), k2=f"近{di}根K线DEA", k3="处于0轴以上", v1=f"是")
+        s[v.key] = v.value
+    elif up == down == 0 and dea[-di] < 0:
+        v = Signal(k1=str(c.freq.value), k2=f"近{di}根K线DEA", k3="处于0轴以下", v1=f"是")
+        s[v.key] = v.value
+
+    return s
+
+def tas_dif_zero_V221106(c: CZSC, di=55) -> OrderedDict:
+    """dif回抽0轴
+
+    信号逻辑：
+    1.首先在基于di根k线为一段向上趋势；
+    2.其次连续di根k线形成的dif最低点在 +—dif最大值0.2倍之间，在此期间判断为上涨过程中回抽0轴
+
+    信号列表：
+    - Signal('15分钟_近55根K线DIF_回抽0轴_否_任意_任意_0')
+    - Signal('15分钟_近55根K线DIF_回抽0轴_是_任意_任意_0')
+
+    :param c: CZSC对象
+    :param di: 连续倒di根K线
+    :return:
+    """
+    dif = [x.cache['MACD']['dif'] for x in c.bars_raw[-di:]]
+
+    s = OrderedDict()
+
+    # 近di根K线的dif的最小值，在正负0.2*dif最大值区间内，即回抽0轴
+    if - max(dif) * 0.2 < min(dif) < max(dif) * 0.2:
+        v = Signal(k1=str(c.freq.value), k2=f"近{di}根K线DIF", k3="回抽0轴", v1=f"是")
+    else:
+        v = Signal(k1=str(c.freq.value), k2=f"近{di}根K线DIF", k3="回抽0轴", v1=f"否")
+    s[v.key] = v.value
+    return s
+
+def macd_zs_v221106(c: CZSC, di=55) -> OrderedDict:
+    """单纯通过价格判断股价突破中枢
+
+    信号逻辑：
+    1.划定[-di:-13]区间为中枢区间，取其高点；
+    2.取倒13根K线的最高点，此笔为中枢突破；
+    3.取倒3笔的低点，判断该点位与中枢的相对位置
+
+    信号列表：
+    - Signal('15分钟_K线价格_冲高回落_中枢之内_任意_任意_0')
+    - Signal('15分钟_K线价格_冲高回落_中枢之上_任意_任意_0')
+
+    :param c: CZSC对象
+    :param di: 连续倒di根K线
+    :return:
+    """
+    s = OrderedDict()
+
+    high_zs = max([x.high for x in c.bars_raw][-di:-13])
+    high_last_bi = max([x.high for x in c.bars_raw][-13:])
+    low_last_bi = min([x.low for x in c.bars_raw][-3:])
+
+    if high_zs < low_last_bi < high_last_bi < high_zs * 1.1:
+        v = Signal(k1=str(c.freq.value), k2='K线价格', k3="冲高回落", v1="中枢之上")
+        s[v.key] = v.value
+    elif high_zs > low_last_bi:
+        v = Signal(k1=str(c.freq.value), k2='K线价格', k3="冲高回落", v1="中枢之内")
+        s[v.key] = v.value
+    else:
+        v = Signal(k1=str(c.freq.value), k2='K线价格', k3="冲高回落", v1="其他")
+        s[v.key] = v.value
+
+    return s
+
+
+from czsc.signals.utils import check_cross_info
+
+
+# 计算倒di根k线Macd面积
+# macd柱子面积背驰
+def tas_macd_area_compare_V221106(c: CZSC, di: int = 55) -> OrderedDict:
+    """MACD柱子面积背驰
+
+    信号逻辑：
+    计算最近di根K线周期中，两次相邻macd柱子面积背驰比较
+
+    信号列表：
+    - Signal('15分钟_绿柱子_背驰_绿柱子_否_任意_0')
+    - Signal('15分钟_绿柱子_背驰_绿柱子_是_任意_0')
+    - Signal('15分钟_红柱子_背驰_红柱子_否_任意_0')
+    - Signal('15分钟_红柱子_背驰_红柱子_是_任意_0')
+
+    :param c: czsc对象
+    :param di:最近di跟K线
+    :return:
+    """
+
+    k1, k2, k3 = f"{c.freq.value}_绿柱子_背驰".split('_')
+    m1, m2, m3 = f"{c.freq.value}_红柱子_背驰".split('_')
+
+    dif = [x.cache['MACD']['dif'] for x in c.bars_raw[-di:]]
+    dea = [x.cache['MACD']['dea'] for x in c.bars_raw[-di:]]
+
+    cross = check_cross_info(dif, dea)
+
+    # 过滤低级别信号抖动造成的金叉死叉(这个参数根据自身需要进行修改）
+    re_cross = [i for i in cross if i['距离'] >= 2]
+    cross_ = []
+
+    if len(re_cross) == 0:
+        pass
+    else:
+        for i in range(0, len(re_cross)):
+            if len(cross_) >= 1 and re_cross[i]['类型'] == re_cross[i - 1]['类型']:
+                # 不将上一个元素加入cross_
+                del cross_[-1]
+
+                # 我这里只重新计算了面积、快慢线的高低点，其他需要重新计算的参数各位可自行编写
+                re_cross[i]['面积'] = re_cross[i - 1]['面积'] + re_cross[i]['面积']
+
+                re_cross[i]['快线高点'] = max(re_cross[i - 1]['快线高点'], re_cross[i]['快线高点'])
+                re_cross[i]['快线低点'] = min(re_cross[i - 1]['快线低点'], re_cross[i]['快线低点'])
+
+                re_cross[i]['慢线高点'] = max(re_cross[i - 1]['慢线高点'], re_cross[i]['慢线高点'])
+                re_cross[i]['慢线低点'] = min(re_cross[i - 1]['慢线低点'], re_cross[i]['慢线低点'])
+
+                cross_.append(re_cross[i])
+            else:
+                cross_.append(re_cross[i])
+
+    # 金叉判断基于剔除抖动的cross_列表
+    gold_x = [i for i in cross_ if i['类型'] == '金叉']
+    die_x = [i for i in cross_ if i['类型'] == '死叉']
+
+    s = OrderedDict()
+    # 最后一次金叉的面积 < 倒数第二次金叉的面积
+    if len(gold_x) >= 2 and gold_x[-1]['面积'] < gold_x[-2]['面积']:
+        v1 = '绿柱子'
+        v2 = '是'
+    else:
+        v1 = '绿柱子'
+        v2 = '否'
+    signal = Signal(k1=k1, k2=k2, k3=k3, v1=v1, v2=v2)
+    s[signal.key] = signal.value
+
+    if len(die_x) >= 2 and die_x[-1]['面积'] < die_x[-2]['面积']:
+        w1 = '红柱子'
+        w2 = '是'
+    else:
+        w1 = '红柱子'
+        w2 = '否'
+    signal = Signal(k1=m1, k2=m2, k3=m3, v1=w1, v2=w2)
+    s[signal.key] = signal.value
+
+    return s
 
 # 定义择时交易策略，策略函数名称必须是 trader_strategy
 # ----------------------------------------------------------------------------------------------------------------------
