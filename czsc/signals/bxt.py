5d147c2ea5d41621aa915963f0a32c3ea546220a
lynxluu <lynxluu@gmail.com>
2023-02-21 23:47:52 +0800

    6笔形态

--- czsc/signals/bxt.py
+++ czsc/signals/bxt.py
@@ -145,6 +145,72 @@ def check_five_bi(bis: List[Union[BI, FakeBI]], freq: Freq, di: int = 1) -> Sign
 
     return v
 
+def check_six_bi(bis: List[Union[BI, FakeBI]], freq: Freq, di: int = 1) -> Signal:
+    """识别七笔形态
+
+    :param freq: K线周期，也可以称为级别
+    :param bis: 由远及近的六笔
+    :param di: 最近一笔为倒数第i笔
+    """
+    di_name = f"倒{di}笔"
+    v = Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='其他', v2='其他', v3='其他')
+
+    if len(bis) != 6:
+        return v
+
+    bi1, bi2, bi3, bi4, bi5, bi6 = bis
+
+    if not (bi1.direction == bi3.direction == bi5.direction):
+        print(f"1,3,5 的 direction 不一致，无法识别五段形态；{bi1}{bi3}{bi5}")
+        return v
+
+    direction = bi1.direction
+    max_high = max([x.high for x in bis])
+    min_low = min([x.low for x in bis])
+    assert direction in [Direction.Down, Direction.Up], "direction 的取值错误"
+
+    # if direction == Direction.Down:
+    #     # aAb式底背驰
+    #     if min(bi2.high, bi4.high) > max(bi2.low, bi4.low) and max_high == bi1.high and bi5.power < bi1.power:
+    #         if (min_low == bi3.low and bi5.low < bi1.low) or (min_low == bi5.low):
+    #             return Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='底背驰', v2='五笔aAb式')
+    #
+    #     # 类趋势底背驰
+    #     if max_high == bi1.high and min_low == bi5.low and bi4.high < bi2.low and bi5.power < max(bi3.power, bi1.power):
+    #         return Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='底背驰', v2='五笔类趋势')
+    #
+    #     # 上颈线突破
+    #     if (min_low == bi1.low and bi5.high > min(bi1.high, bi2.high) > bi5.low > bi1.low) \
+    #             or (min_low == bi3.low and bi5.high > bi3.high > bi5.low > bi3.low):
+    #         return Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='上颈线突破', v2='五笔')
+    #
+    #     # 五笔三买，要求bi5.high是最高点
+    #     if max_high == bi5.high > bi5.low > max(bi1.high, bi3.high) \
+    #             > min(bi1.high, bi3.high) > max(bi1.low, bi3.low) > min_low:
+    #         return Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='类三买', v2='五笔')
+
+    if direction == Direction.Up:
+        # 三买只找方向朝上的，bi6.low 回调不破 中枢的高点 min(bi2.high, bi4.high), 且bi5的power比1，3大，bi6的power比2，4大
+        # aAb式类一卖
+        if min(bi2.high, bi4.high) > max(bi2.low, bi4.low) and min_low == bi1.low and bi5.power < bi1.power:
+            if (max_high == bi3.high and bi5.high > bi1.high) or (max_high == bi5.high):
+                return Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='顶背驰', v2='五笔aAb式')
+
+        # 类趋势类一卖
+        if min_low == bi1.low and max_high == bi5.high and bi5.power < max(bi1.power, bi3.power) and bi4.low > bi2.high:
+            return Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='顶背驰', v2='五笔类趋势')
+
+        # 下颈线突破
+        if (max_high == bi1.high and bi5.low < max(bi1.low, bi2.low) < bi5.high < max_high) \
+                or (max_high == bi3.high and bi5.low < bi3.low < bi5.high < max_high):
+            return Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='下颈线突破', v2='五笔')
+
+        # 五笔三卖，要求bi5.low是最低点
+        if min_low == bi5.low < bi5.high < min(bi1.low, bi3.low) \
+                < max(bi1.low, bi3.low) < min(bi1.high, bi3.high) < max_high:
+            return Signal(k1=freq.value, k2=di_name, k3='基础形态', v1='类三卖', v2='五笔')
+
+    return v
 
 def check_seven_bi(bis: List[Union[BI, FakeBI]], freq: Freq, di: int = 1) -> Signal:
     """识别七笔形态
