d0b5ed5db5b2e276eb6beed29f8aa5f8dd90591e
lynxluu <lynxluu@gmail.com>
2023-04-18 21:56:16 +0800

    从文件读取k线正确

--- examples/strategies/lynx/gpt_3bs.py
+++ examples/strategies/lynx/gpt_3bs.py
@@ -1,27 +1,50 @@
 # python
 # 获取最近250根K线
+import pandas as pd
 from pytdx.hq import TdxHq_API
 from pytdx.hq import *
 from pytdx.params import *
+from pytdx.reader import *
+import pandas as pd
 
 
 # 使用PyTDx获取上证指数的最新250根日线数据
-api = TdxHq_API()
-N = 250
-with api.connect('218.75.126.9', 7709):
-    data = api.get_security_bars(TDXParams.KLINE_TYPE_DAILY, TDXParams.MARKET_SH, '000001', 0, N)
-    # data = api.to_df(api.get_security_bars(TDXParams.KLINE_TYPE_DAILY, TDXParams.MARKET_SH, '000001', 0, N))
-print(data[0:3])
+def read_ol():
+    api = TdxHq_API()
+    N = 250
+
+    with api.connect('218.75.126.9', 7709):
+        data = api.get_security_bars(TDXParams.KLINE_TYPE_DAILY, TDXParams.MARKET_SH, '000001', 0, N)
+        df = api.to_df(data)
+        # df = df.sort_values('datetime')
+        # df = api.to_df(api.get_security_bars(TDXParams.KLINE_TYPE_DAILY, TDXParams.MARKET_SH, '000001', 0, N))
+    return df
+
+# 从文件读取日线
+def read_file():
+    path = r'D:\app_dev\tdx\vipdoc\sh\lday\sh000001.day'
+    api = TdxDailyBarReader()
+    klines = api.get_df_by_file(path)[-250:]
+
+    return klines
+
+
+
+# df = read_ol()
+df = read_file()
+# print(df)
+# print(df[:10])
+# print(df[240:])
 
 # 初始化存储分型的fx数组
 fx = []
 
 # 遍历最近249根K线(从第2根开始)
-for i in range(2, len(data)-1):
+for i in range(2, len(df)-1):
     # 获取当前K线、前一根K线和后一根K线的最高价和最低价
-    pre = data.iloc[i-1]
-    curr = data.iloc[i]
-    aft = data.iloc[i+1]
+    pre = df.iloc[i-1]
+    curr = df.iloc[i]
+    aft = df.iloc[i+1]
 
     # current_high = data.loc[i, 'high']
     # current_low = data.loc[i, 'low']
@@ -37,6 +60,6 @@ for i in range(2, len(data)-1):
     #     fx.append([0,i])  # 底分型
     # if max(curr)
 
-    print[curr.loc('high')]
+    print[curr]
 # 输出fx数组
-# print(fx)
+print(fx)
